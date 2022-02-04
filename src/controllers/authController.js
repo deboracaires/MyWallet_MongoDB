585c74a9c29d046d1f1fd06444da6aed2a682098
deboracaires <deboracaires@pop-os.localdomain>
2022-02-04 12:14:47 -0300

    roa post sign-up finalizada

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,26 @@
 import db from '../connection/connection.js';
+import bcrypt from 'bcrypt';
 
 export async function SignUp(req, res) {
   try {
-    await db.collection('users').find();
-    res.sendStatus(200)
+    const user = req.body;
+
+    const verifyEmail = await db.collection('users').findOne({ email: user.email});
+
+    if (verifyEmail) {
+        return res.sendStatus(409);
+    }
+
+    const passwordHash = bcrypt.hashSync(user.password, 10);
+
+    await db.collection('users').insertOne({
+        name: user.name,
+        email: user.email,
+        password: passwordHash,
+    });
+   
+    res.sendStatus(201);
   } catch (err) {
-      console.log(err)
     res.sendStatus(500)
   }
 };
